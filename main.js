b1ae2ca6f8194dd5a429ed81585da363c8c351da
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-01-21 19:30:37 +0100

    second stage

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ const endTime = new Date("2022-01-01 00:00:00").getTime();
 
 setInterval(() => {
   const nowTime = new Date().getTime();
-  const time = Math.floor((endTime - nowTime) / 1000);
-  console.log(time);
+  const time = endTime - nowTime;
+  const days = Math.floor(time / 1000 / 60 / 60 / 24);
+  const hours =
+    Math.floor((time / 1000 / 60 / 60) % 24) < 10
+      ? `0${Math.floor((time / 1000 / 60 / 60) % 24)}`
+      : Math.floor((time / 1000 / 60 / 60) % 24);
+  const minutes =
+    Math.floor((time / 1000 / 60) % 60) < 10
+      ? `0${Math.floor((time / 1000 / 60) % 60)}`
+      : Math.floor((time / 1000 / 60) % 60);
+  const seconds =
+    Math.floor((time / 1000) % 60) < 10 ? `0${Math.floor((time / 1000) % 60)}` : Math.floor((time / 1000) % 60);
+
+  document.querySelector(`.d`).textContent = days;
+  document.querySelector(`.h`).textContent = hours;
+  document.querySelector(`.m`).textContent = minutes;
+  document.querySelector(`.s`).textContent = seconds;
 }, 1000);
